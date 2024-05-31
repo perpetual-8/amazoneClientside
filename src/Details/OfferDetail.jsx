cdb15d8a21e9e728d6ed1692749efdadbbe4ddcb
perpetual-8 <hamzaabhutta@gmail.com>
2024-05-31 06:09:36 -0700

    hoja.

---
+++ src/Details/OfferDetail.jsx
@@ -0,0 +1,68 @@
+export const AmazonPay = [
+    {
+        id: 1,
+        offer: 'Get up to 10% off* on flight tickets',
+        offerImage: '../../public/image/ProductImage/s3.jpg'
+    },
+    {
+        id: 2,
+        offer: 'Zero gateway fees on trains',
+        offerImage: '../../public/image/ProductImage/s1.jpg'
+    },
+    {
+        id: 3,
+        offer: 'Guaranteed rewards on bus tickets',
+        offerImage: '../../public/image/ProductImage/s2.jpg'
+    },
+    {
+        id: 4,
+        offer: 'Products for your travel needs',
+        offerImage: '../../public/image/ProductImage/s4.jpg'
+    },
+]
+
+export const Upcominglaunches = [
+    {
+        id: 1,
+        offer: 'OnePlus Nord CE 3 Lite 5G | ₹19,999',
+        offerImage: '../../public/image/ProductImage/s5.jpg'
+    },
+    {
+        id: 2,
+        offer: 'Samsung M14 5G | ₹14,990',
+        offerImage: '../../public/image/ProductImage/s6.jpg'
+    },
+    {
+        id: 3,
+        offer: 'Realme narzo N55 | ₹10,999',
+        offerImage: '../../public/image/ProductImage/s7.jpg'
+    },
+    {
+        id: 4,
+        offer: 'Explore all new launches',
+        offerImage: '../../public/image/ProductImage/s8.jpg'
+    },
+]
+
+export const StylesForMen = [
+    {
+        id: 1,
+        offer: 'Clothing',
+        offerImage: '../../public/image/ProductImage/s9.jpg'
+    },
+    {
+        id: 2,
+        offer: 'Footwear',
+        offerImage: '../../public/image/ProductImage/s10.jpg'
+    },
+    {
+        id: 3,
+        offer: 'Watches',
+        offerImage: '../../public/image/ProductImage/s11.jpg'
+    },
+    {
+        id: 4,
+        offer: 'Bags & luggage',
+        offerImage: '../../public/image/ProductImage/s12.jpg'
+    },
+]
